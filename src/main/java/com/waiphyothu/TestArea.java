15ae91e19240489a0aeb9ed57f05f3d6708c0772
WP <wp@Hsus-MacBook-Air.local>
2024-04-21 16:04:47 +0700

    Shell sort algorithm (improvement of Insertion Sort)

---
+++ src/main/java/com/waiphyothu/TestArea.java
@@ -0,0 +1,19 @@
+package com.waiphyothu;
+
+public class TestArea {
+
+    public static void main(String[] args) {
+
+        int[] intArray = {20, 5, -15, 7, 55, 1, -22};
+
+        int gap = 7;
+
+
+
+        System.out.println( gap/=2);
+
+
+        System.out.println( gap);
+
+    }
+}
