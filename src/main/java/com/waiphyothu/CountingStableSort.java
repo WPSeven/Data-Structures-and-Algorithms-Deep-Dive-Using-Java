92c62cdd8864a0ce25d46ebc30dc18c5789b460c
WP <wp@Hsus-MacBook-Air.local>
2024-04-24 22:56:33 +0700

    Radix Sort et   x Sort 👍

---
+++ src/main/java/com/waiphyothu/CountingStableSort.java
@@ -0,0 +1,18 @@
+package com.waiphyothu;
+
+public class CountingStableSort {
+
+    public static void main(String[] args) {
+
+        int[] intArray = {2, 5, 9, 8, 2, 8, 7, 10, 4, 3};
+
+        //todo
+
+        for (int i = 0; i < intArray.length; i++){
+            System.out.println(intArray[i]);
+        }
+
+    }
+
+
+}
--- src/main/java/com/waiphyothu/TestArea.java
+++ src/main/java/com/waiphyothu/TestArea.java
@@ -10,10 +10,16 @@ public class TestArea {
 
 
 
-        System.out.println( gap/=2);
+        System.out.println( gap/=2 );
 
 
         System.out.println( gap);
 
+
+        System.out.println(getDigit(0, 1024, 10));
+    }
+
+    public static int getDigit(int position, int value, int radix) {
+        return value / (int) Math.pow(radix, position) % radix;
     }
 }
